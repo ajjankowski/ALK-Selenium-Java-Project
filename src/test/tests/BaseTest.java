a5d14fc6ad11a70170b0050a53323e9bdf971224
ajjankowski <aj.jankowski92@gmail.com>
2022-04-02 17:16:26 +0200

    BaseTest

--- src/test/tests/BaseTest.java
+++ src/test/tests/BaseTest.java
@@ -2,14 +2,13 @@ package tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.ScreenshotTaker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import utils.ScreenshotTaker;
 
 import java.util.concurrent.TimeUnit;
 
