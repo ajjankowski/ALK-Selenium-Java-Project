560d8c4162489bb25ba65378feeded1289f8b968
ajjankowski <aj.jankowski92@gmail.com>
2022-04-02 16:54:58 +0200

    Initial commit

---
+++ src/test/tests/BaseTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.ScreenshotTaker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected static WebDriver driver;
+    protected static ScreenshotTaker screenshotTaker = new ScreenshotTaker();
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentReports extentReports;
+
+    @BeforeSuite
+    public static void beforeClass() {
+        htmlReporter = new ExtentHtmlReporter("TestReport.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @AfterSuite
+    public static void afterClass() {
+        htmlReporter.flush();
+        extentReports.flush();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://joemonster.org/logowanie");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
